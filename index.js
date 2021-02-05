4f5069db0ebe089e3409d49e65065e465ffbf739
Isabel Laurenceau <isalau@ufl.edu>
2021-02-05 10:13:15 -0500

    Update index.js

--- index.js
+++ index.js
@@ -3,5 +3,6 @@ var app = express()
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
+	console.log('Accessing the secret section ...')
   res.send('hello world')
 })
